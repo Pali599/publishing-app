71056b6de9986ebe74246918ade76f7ee1b77657
Pali599 <pavol.melko@gmail.com>
2023-04-09 16:38:39 +0200

    zmeny v databaze - pridavanie cudzich klucov a zmeny nazvov atributov

---
+++ database/migrations/2023_04_09_142854_change_default_value_role_id_in_users_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->unsignedBigInteger('role_id')->default(2)->change();
+            $table->unsignedBigInteger('type_id')->default(0)->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->int('role_id')->change();
+            $table->string('type_id')->change();
+        });
+    }
+};
